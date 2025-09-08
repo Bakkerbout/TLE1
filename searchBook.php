51f7933586d722aca5563de1f4e2b609c37d8ab8
maudd <1091860@hr.nl>
2025-09-08 14:48:02 +0200

    ADD background image

--- searchBook.php
+++ searchBook.php
@@ -1,1 +1,16 @@
 <?php
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Search book</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+<main>
+    <div class="background-img"></div>
+</main>
+</body>
+</html>
