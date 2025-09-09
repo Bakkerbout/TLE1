91a60bea35a01aac37ced463a5c256a381dc0fff
maudd <1091860@hr.nl>
2025-09-09 14:02:44 +0200

    ADD book details

--- bookDetails.php
+++ bookDetails.php
@@ -1,1 +1,28 @@
 <?php
+if (isset($_GET['id'])) {
+    $bookId = $_GET['id'];
+    $apiUrl = "https://www.googleapis.com/books/v1/volumes/$bookId";
+    $book = json_decode(file_get_contents($apiUrl), true);
+}
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Book details</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script type="text/javascript" src="js/bookDetails.js"></script>
+</head>
+<body>
+<main>
+    <a href="index.php">Terug naar boekenkast</a>
+    <div class="background-img"></div>
+    <div class="shelfBackground">
+        <div class="shelf" id="shelf-background">
+            <h1><?php echo $book['volumeInfo']['title']; ?></h1>
+        </div>
+        <a href="index.php">Klaar met lezen</a>
+    </div>
+</main>
+</body>
+</html>
--- js/main.js
+++ js/main.js
@@ -23,46 +23,31 @@ function loadBooks() {
             data.items.forEach((book, i) => {
                 const title = book.volumeInfo.title;
                 const thumbnail = book.volumeInfo.imageLinks?.thumbnail;
+                const bookId = book.id;
 
                 let bookDiv = document.createElement("div");
                 bookDiv.className = "book-spine";
-                bookDiv.textContent = title;
 
-                shelves[shelfIndex].appendChild(bookDiv);
+                if (thumbnail) {
+                    let img = document.createElement("img");
+                    img.src = thumbnail;
+                    img.alt = title;
+                    bookDiv.appendChild(img);
+                }
+
+                let bookTitle = document.createElement("p");
+                bookTitle.textContent = title;
+                bookDiv.appendChild(bookTitle);
+
+                let link = document.createElement("a");
+                link.href = `bookDetails.php?id=${bookId}`;
+                link.appendChild(bookDiv);
+
+                shelves[shelfIndex].appendChild(link);
 
                 if ((i + 1) % 10 === 0 && shelfIndex < shelves.length - 1) {
                     shelfIndex++;
                 }
             });
         });
-
 }
-
-// fetch(apiUrl)
-//     .then(res => res.json())
-//     .then(data => {
-//         const shelves = [
-//             document.getElementById("shelf-1"),
-//             document.getElementById("shelf-2"),
-//             document.getElementById("shelf-3"),
-//             document.getElementById("shelf-4"),
-//             document.getElementById("shelf-5")
-//         ];
-//
-//         let shelfIndex = 0;
-//
-//         data.items.forEach((book, i) => {
-//             const title = book.volumeInfo.title;
-//             const thumbnail = book.volumeInfo.imageLinks?.thumbnail;
-//
-//             let bookDiv = document.createElement("div");
-//             bookDiv.className = "book-spine";
-//             bookDiv.textContent = title;
-//
-//             shelves[shelfIndex].appendChild(bookDiv);
-//
-//             if ((i + 1) % 10 === 0 && shelfIndex < shelves.length - 1) {
-//                 shelfIndex++;
-//             }
-//         });
-//     });
